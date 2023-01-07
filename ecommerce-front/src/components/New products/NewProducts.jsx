02d22699f0ca8bf00e2b930529afbd3920aa0025
Julian Lescano <lescano.julian22@gmail.com>
2023-01-07 10:55:40 -0300

    new products component created

--- ecommerce-front/src/components/New products/NewProducts.jsx
+++ ecommerce-front/src/components/New products/NewProducts.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCreateProductMutation } from '../../services/appApi';
+import styles from './newProducts.module.css'
+
 
 function NewProducts() {
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [images, setImages] = useState([]);
+  const [imageToRemove, setImageToRemove] = useState(null);
+  const navigate = useNavigate();
+  const [createProduct, {isError, error, isLoading, isSucces}] = useCreateProductMutation();
   return (
     <div>
       New Product
