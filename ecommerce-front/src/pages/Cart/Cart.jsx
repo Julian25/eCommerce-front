3d7e05484bd6c8206ee624957aeb1614a60c110f
Julian Lescano <lescano.julian22@gmail.com>
2023-01-11 20:11:29 -0300

    add, remove, increment and decrease product from cart working correctly, payments started

--- ecommerce-front/src/pages/Cart/Cart.jsx
+++ ecommerce-front/src/pages/Cart/Cart.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import styles from './cart.module.css';
 import { useSelector } from 'react-redux';
 import { Alert, Container, Row, Col, Table } from 'react-bootstrap';
+import { 
+  useIncreaseCartProductMutation, 
+  useDecreaseCartProductMutation,
+  useRemoveFromCartMutation 
+} from '../../services/appApi';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js'
 
+const stripePromise = loadStripe('pk_test_51MPD8dKBwGKOp0r6AfPVovlvmJCH9jjkt7a9IoZgbP1cd3xkIqoMBTTiFquLXW8xGIGhevTpfOe83JWza8sjTlzl00KJK7QmcY');
 
 function Cart() {
 
@@ -10,6 +18,9 @@ function Cart() {
     const products = useSelector( state => state.products);
     const userCartObj = user.cart;
     let cart = products.filter((product) => userCartObj[product._id] != null);
+    const [increaseCart] = useIncreaseCartProductMutation();
+    const [decreaseCart] = useDecreaseCartProductMutation();
+    const [removeFromCart, { isLoading }] = useRemoveFromCartMutation()
 
   return (
     <Container className={styles.cart__container}>
@@ -18,9 +29,9 @@ function Cart() {
           <h1 className='pt-2 h3'>Shopping cart</h1>
           {cart.length == 0 ? (
               <Alert variant='info'>Shopping cart is empty. Add products to your cart</Alert>
-          ) : ( 
-          <div>Payment here</div>
-          ) }
+          ) : (
+            <Elements stripe={stripePromise}></Elements>
+            ) }
         </Col>
         <Col md={5}>
           {cart.length > 0 && (
@@ -40,15 +51,19 @@ function Cart() {
                     <tr>
                       <td>&nbsp;</td>
                       <td>
-                        <i className={`fa fa-times ${styles.icon__remove}`}></i>
+                        {!isLoading && <i className={`fa fa-times ${styles.icon__remove}`} onClick={(e) => removeFromCart({productId: item._id, 
+                          price: item.price, userId: user._id})}></i>}
                         <img src={item.pictures[0].url} className={styles.product__img}/>
                       </td>
                       <td>{item.price}</td>
                       <td>
                         <span className={styles.quantity__indicator}>
-                          <i className={`fa fa-minus-circle ${styles.quantity__button} ${styles.minus}`}></i>
+                          {user.cart[item._id] >= 2 && <i className={`fa fa-minus-circle ${styles.quantity__button} ${styles.minus}`} 
+                          onClick={(e) => decreaseCart({productId: item._id, price: item.price, userId: user._id})}></i> }
                           <span>{user.cart[item._id]}</span>
-                          <i className={`fa fa-plus-circle ${styles.quantity__button } ${styles.plus}`}></i>
+                          <i className={`fa fa-plus-circle ${styles.quantity__button } ${styles.plus}`} onClick={(e) => increaseCart({
+                            productId: item._id, price: item.price, userId: user._id
+                          })}></i>
                         </span>
                       </td>
                       <td>{item.price * user.cart[item._id]}</td>
--- ecommerce-front/src/services/appApi.js
+++ ecommerce-front/src/services/appApi.js
@@ -40,7 +40,7 @@ export const appApi = createApi({
             query: (body) => ({
                 url: "/products/remove-from-cart",
                 body,
-                method: "DELETE",
+                method: "POST",
             }),
         }),
 
